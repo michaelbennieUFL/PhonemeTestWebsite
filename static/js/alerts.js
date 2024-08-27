2674765871a77ca85206e051f7dd515f06ee1c4c
michaelbennie <michaelbennie@ufl.edu>
2024-08-27 04:13:29 -0400

    Add auto-dismiss functionality for alerts
    Included a new `alerts.js` script to automatically fade and remove alert messages after 5 seconds. Updated `baseImports.html` to link to the new script located in the `static/js` directory.

---
+++ static/js/alerts.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const alerts = document.querySelectorAll('.alert');
+    alerts.forEach(alert => {
+        setTimeout(() => {
+            alert.classList.remove('show');
+            alert.classList.add('fade');
+            setTimeout(() => {
+                alert.remove();
+            }, 100);
+        }, 5000);
+    });
+});
