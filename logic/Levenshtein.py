ba343c09949224c735771e7a6766e8bde7c57b20
michaelbennie <michaelbennie@ufl.edu>
2024-09-10 04:08:41 -0400

    Fix typo in variable name in Levenshtein distance function
    Correct the variable name from "original_sring" to "original_string" in the `levenshtein_distance` function. This ensures accurate referencing and calculations within the distance matrix and subsequent function calls.

--- logic/Levenshtein.py
+++ logic/Levenshtein.py
@@ -38,23 +38,23 @@ def edit_path(dist_matrix, str1, str2):
         "separated_str2": separated_str2
     }
 
-def levenshtein_distance(original_sring="", new_string="", mismatch_cost=1, delete_insert_cost=1, ):
-    dist_matrix = [[None] * (len(new_string) + 1) for _ in range(len(original_sring) + 1)]
-    for i in range(len(original_sring) + 1):
+def levenshtein_distance(original_string="", new_string="", mismatch_cost=1, delete_insert_cost=1, ):
+    dist_matrix = [[None] * (len(new_string) + 1) for _ in range(len(original_string) + 1)]
+    for i in range(len(original_string) + 1):
         for j in range(len(new_string) + 1):
             if i == 0:
                 dist_matrix[i][j] = j * delete_insert_cost
             elif j == 0:
                 dist_matrix[i][j] = i * delete_insert_cost
             else:
-                current_dist = 0 if original_sring[i - 1] == new_string[j - 1] else mismatch_cost
+                current_dist = 0 if original_string[i - 1] == new_string[j - 1] else mismatch_cost
                 mismatch_dist = current_dist + dist_matrix[i - 1][j - 1]
                 delete_dist = delete_insert_cost + dist_matrix[i - 1][j]
                 insert_dist = delete_insert_cost + dist_matrix[i][j - 1]
                 dist_matrix[i][j] = min(mismatch_dist, delete_dist, insert_dist)
 
-    best_dist = dist_matrix[len(original_sring)][len(new_string)]
-    edit_path_result = edit_path(dist_matrix, original_sring, new_string)
+    best_dist = dist_matrix[len(original_string)][len(new_string)]
+    edit_path_result = edit_path(dist_matrix, original_string, new_string)
 
     return {
         "best_dist": best_dist,
